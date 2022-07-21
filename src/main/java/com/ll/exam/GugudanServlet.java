4c0927924eb9a06464422785b2bc9665f6dd7e3d
SG1515 <sunggu507@naver.com>
2022-07-21 16:04:55 +0900

    구구단을 model2 방식으로 처리

--- src/main/java/com/ll/exam/GugudanServlet.java
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -1,26 +1,36 @@
 package com.ll.exam;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
 @WebServlet("/gugudan")
 public class GugudanServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)  {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         Rq rq = new Rq(req, resp);
 
+
+
         //URL에서 gugudan?dan=4&limit=5 4단 곱하기 5까지
-        int dan = rq.getIntParam("dan", 0); //문자열 입력 형변환
-        int limit = rq.getIntParam("limit", 0);
+        int dan = rq.getIntParam("dan", 9); //문자열 입력 형변환
+        int limit = rq.getIntParam("limit", 9);
 
-        rq.appendBody("<h2>%d단</h2>".formatted(dan));
+        // request에 정보를 담는다.
+        // 왜냐하면 나중에 gugudan2.jsp에서 해당 내용을 꺼낼 수 있기 때문에
+        req.setAttribute("dan", dan);
+        req.setAttribute("limeit", limit);
 
-        for(int i = 1; i <= limit; i++){
-            rq.appendBody("<div>%d * %d = %d</div>\n".formatted(dan, i, dan*i));
-        }
+
+        // gugudan2.jsp 에게 나머지 작업을 토스
+        RequestDispatcher requestDispatcher = req.getRequestDispatcher("/gugudan2.jsp");
+        requestDispatcher.forward(req, resp);
 
 
 
