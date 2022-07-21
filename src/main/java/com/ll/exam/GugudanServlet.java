270736d627bf021f4defecd44e650e983699ee83
SG1515 <sunggu507@naver.com>
2022-07-21 13:09:46 +0900

    구구단 서블릿 추가

--- src/main/java/com/ll/exam/GugudanServlet.java
+++ src/main/java/com/ll/exam/GugudanServlet.java
@@ -8,14 +8,21 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
+@WebServlet("/gugudan")
+public class GugudanServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8"); // 들어오는 데이터를 UTF-8 로 해석하겠다.
         resp.setCharacterEncoding("UTF-8"); // 완성되는 HTML의 인코딩을 UTF-로 하겠다.
         resp.setContentType("text/html; charset=utf-8"); // 브라우저에게 우리가 만든 결과물이 UTF-8 이다 라고 알리는 의미
 
-        resp.getWriter().append("1234");
+        int dan = 8;
+        int limit = 5; //5단까지
+
+        resp.getWriter().append("<h2>%d단</h2>".formatted(dan));
+
+        for(int i = 1; i <= limit; i++){
+            resp.getWriter().append("<div>%d * %d = %d</div>\n".formatted(dan, i, dan*i));
+        }
     }
 }
